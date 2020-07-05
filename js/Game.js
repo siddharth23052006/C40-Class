3d4b042e2fc05f5462ca218ac97f66fe39700e44
siddharth23052006 <siddharthshivakumar2@gmail.com>
2020-07-05 11:58:41 +0530

    Added track background and car images

--- js/Game.js
+++ js/Game.js
@@ -30,9 +30,17 @@ class Game {
     }
 
     car1 = createSprite(100,200);
+    car1.addImage("car1",car1Img);
+
     car2 = createSprite(300,200);
+    car2.addImage("car2",car2Img);
+
     car3 = createSprite(500,200);
+    car3.addImage("car3",car3Img);
+
     car4 = createSprite(700,200);
+    car4.addImage("car4",car4Img);
+
     cars = [car1, car2, car3, car4];
   }
 
@@ -42,13 +50,14 @@ class Game {
     Player.getPlayerInfo();
     
     if(allPlayers !== undefined){
-      //var display_position = 100;
+      background("brown");
+      image(trackImg,0,-displayHeight*4,displayWidth,displayHeight*5);
       
       //index of the array
       var index = 0;
 
       //x and y position of the cars
-      var x = 0;
+      var x = 210;
       var y;
 
       for(var plr in allPlayers){
@@ -56,7 +65,7 @@ class Game {
         index = index + 1 ;
 
         //position the cars a little away from each other in x direction
-        x = x + 200;
+        x = x + 230;
         //use data form the database to display the cars in y direction
         y = displayHeight - allPlayers[plr].distance;
         cars[index-1].x = x;
@@ -78,7 +87,15 @@ class Game {
       player.distance +=10
       player.update();
     }
+    if (player.distance > 4300){
+      gameState = 2;
+      //console.log(player.distance);
+    }
 
     drawSprites();
+  }
+  end(){
+    console.log("Game has Ended");
+    game.update(2);
   }
 }
